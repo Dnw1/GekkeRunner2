74a63b2b840ed3e8288fc495d6d756b3b812049c
Dylan Waij <d_way@live.nl>
2016-04-11 13:27:20 +0200

    Menu placeholder

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene("Game");
+    }
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
