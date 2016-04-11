1141430b20fa849c55a58743b8a1a83d817550c9
ezowolf <wiggler@live.nl>
2016-04-11 17:44:32 +0200

    Day 1 Update

--- Assets/Scripts/PlayerObstacleCollision.cs
+++ Assets/Scripts/PlayerObstacleCollision.cs
@@ -6,12 +6,20 @@ public class PlayerObstacleCollision : MonoBehaviour {
 	[SerializeField]
 	private ScreenShake screenShake;
 
+	private Vector2 startPosition;
+
+	void Start()
+	{
+		startPosition = this.transform.position;
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == TagData.obstacleTag) 
 		{
 			Debug.Log ("AUW");
 			screenShake.StartShake ();
+			this.transform.position = startPosition;
 		}
 	}
 }
