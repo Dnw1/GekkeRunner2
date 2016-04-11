b25f951ffd78f659904537772319bd2eb085d4a5
jeroen avis <jeroenavis123@gmail.com>
2016-04-11 14:38:44 +0200

    Playermovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float Speed = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+
+	void FixedUpdate(){
+		movex = Input.GetAxis ("Horizontal");
+		movey = Input.GetAxis ("Vertical");
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (movex * Speed, movey * Speed);
+	}
+
+
+
+
+}	
