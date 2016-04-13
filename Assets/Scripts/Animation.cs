bbe95cf75773694c1958d1d8730f7db30e07c4c0
JherSter <jeroenavis123@gmail.com>
2016-04-13 13:22:49 +0200

    Animations

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.tag == "Player") {
+			animator.SetTrigger ("HitPlane");
+			Destroy (gameObject, 0.7f);
+	}
+	}
+}
