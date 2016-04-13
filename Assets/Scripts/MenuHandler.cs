dd76955afb0b089bbb33accb55d54226d83fd273
WolfFire404 <reillyvas9@live.nl>
2016-04-13 14:35:01 +0200

    In game music

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -4,25 +4,32 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 #endif
 public class MenuHandler : MonoBehaviour {
+    [SerializeField]
+    private AudioSource Ad;
 
     public void StartGame() {
         SceneManager.LoadScene("FinalGameScene");
+        Ad.Play();
     }
     public void Controls()
     {
         SceneManager.LoadScene("Controls");
+        Ad.Play();
     }
     public void Credits()
     {
         SceneManager.LoadScene("Credits");
+        Ad.Play();
     }
     public void MainMenu()
     {
         SceneManager.LoadScene("Menu");
+        Ad.Play();
     }
     public void Quit()
     {
         Application.Quit();
+        Ad.Play();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
