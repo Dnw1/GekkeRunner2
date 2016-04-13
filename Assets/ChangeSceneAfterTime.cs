a43aba0c112e736e1cc4e771ca815dae20a2fa77
ezowolf <wiggler@live.nl>
2016-04-13 11:45:30 +0200

    Win and lose screen

---
+++ Assets/ChangeSceneAfterTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+public class ChangeSceneAfterTime : MonoBehaviour {
+
+	[SerializeField]
+	private  float timeToWait;
+
+	[SerializeField]
+	private string sceneToLoad;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (ChangeScene ());
+	}
+	
+	IEnumerator ChangeScene()
+	{
+		yield return new WaitForSeconds(timeToWait);
+
+		SceneManager.LoadScene (sceneToLoad);
+	}
+}
